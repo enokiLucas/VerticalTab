db49cb65b637b3b80fcaeb716f90e6e12914e2ac
Lucas Enoki <enoki.lucas@gmail.com>
2024-06-25 19:40:17 -0300

    Rewrite the code in Web Components Standard

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-let tabs = document.querySelectorAll(".tabs h3");
-let tabContents = document.querySelectorAll(".tab-content div");
+class VerticalTabsPanel extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
 
-tabs.forEach((tab, index) => {
-	tab.addEventListener("click", () => {
-		tabContents.forEach(content => {
-			content.classList.remove("active");
+	async connectedCallback() {
+		await this.loadHTML(this.shadowRoot, './VerticalTabsPanel.html');
+		await this.loadStyles(this.shadowRoot, './styles.css');
+		this.switchTabs();
+	}
+
+	//Loads the HTML copied from utils/HTMLLoader.js
+	async loadHTML(shadowRoot, htmlPath) {
+		const html = await fetch(htmlPath).then(response => response.text());
+		shadowRoot.innerHTML = html;
+	}
+
+	//Loads the styles, copied from utils/StyleLoader.js
+	async loadStyles(shadowRoot, stylePath) {
+		const css = await fetch(stylePath).then(response => response.text());
+		const style = document.createElement('style');
+		style.textContent = css;
+		shadowRoot.appendChild(style);
+	}
+
+	switchTabs() {
+		const tabs = this.shadowRoot.querySelectorAll(".tabs h3");
+		const tabContents = this.shadowRoot.querySelectorAll(".tab-content div");
+
+		tabs.forEach((tab, index) => {
+			tab.addEventListener("click", () => {
+				tabContents.forEach(content => {
+					content.classList.remove("active");
+				});
+				tabs.forEach((tab) => {
+					tab.classList.remove("active");
+				});
+				tabContents[index].classList.add("active");
+				tabs[index].classList.add("active");
+			});
 		});
-		tabs.forEach((tab) => {
-			tab.classList.remove("active");
-		});
-		tabContents[index].classList.add("active");
-		tabs[index].classList.add("active");
-	});
-});
+	}
+}
+
+customElements.define('vertical-tabs-panel', VerticalTabsPanel);
